b27279b19ffb9c4db80478ca36b27f5928e707ad
h_djebli <hamzad.cne@gmail.com>
2021-05-11 23:56:20 +0100

    initial commit

---
+++ routes/api.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+
+Route::get('hello', function () {
+    return 'Hello World laravel';
+});
+
+
+Route::get('user/{id}', function ($id) {
+    return 'User '.$id;
+});
+
+Route::post('test', function () {
+    return 'this is a test';
+});
+
+//Route::get('order', 'OrderController@show');
+Route::apiResource('orders', 'OrderController');
